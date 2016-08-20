af38c6ad1eba8f8cf21333f824785db936b2a7ec
skull1 <markobest@live.it>
2016-08-20 16:20:28 +0200

    prima provagit/eclipse

---
+++ CercaCollega/src/cercacollega/Login.java
@@ -0,0 +1,12 @@
+package cercacollega;
+
+public class Login {
+	 
+    public static boolean authenticate(String username, String password) {
+        // hardcoded username and password
+        if (username.equals("prova") && password.equals("pass")) {
+            return true;
+        }
+        return false;
+    }
+}
